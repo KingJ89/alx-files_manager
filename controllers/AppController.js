f1aed46bf024256244c9dab66a2fd0477b8ae528
KingJ89 <janmutewera@gmail.com>
2025-01-17 22:56:48 +0000

    task app.js completed successfullu

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,38 +1,18 @@
-import redisClient from '../utils/redis';
-import dbClient from '../utils/db';
+import client from '../utils/redis';
+import clientDb from '../utils/db';
 
-/**
- * Controller class for application-level operations.
- */
 export default class AppController {
-  /**
-   * Returns the status of Redis and database connections.
-   * @param {Object} req The HTTP request object.
-   * @param {Object} res The HTTP response object.
-   */
   static getStatus(req, res) {
     res.status(200).json({
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
+      redis: client.isAlive(),
+      db: clientDb.isAlive(),
     });
   }
 
-  /**
-   * Retrieves the total counts of users and files in the system.
-   * @param {Object} req The HTTP request object.
-   * @param {Object} res The HTTP response object.
-   */
-  static async getStats(req, res) {
-    try {
-      const [usersCount, filesCount] = await Promise.all([
-        dbClient.nbUsers(),
-        dbClient.nbFiles(),
-      ]);
-
-      res.status(200).json({ users: usersCount, files: filesCount });
-    } catch (error) {
-      console.error('Error retrieving stats:', error);
-      res.status(500).json({ error: 'Failed to retrieve stats' });
-    }
+  static getStats(req, res) {
+    Promise.all([clientDb.nbUsers(), clientDb.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
 }
